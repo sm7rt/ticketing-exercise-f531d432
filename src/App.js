11f1123ccdb990bc728fb8a045d04da923159c2c
mitchell <mit7miller@gmail.com>
2023-12-15 11:25:21 -0500

    completed user functionality in ticket reservation exercise

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { MovieList } from "./components/MovieList";
-import { MovieShowtimes } from "./components/MovieShowtimes";
-import { SeatReservation } from "./components/SeatReservation";
-import { PageNotFound } from "./components/PageNotFound";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<MovieList />} />
-        <Route path="/showtimes/:movieId" element={<MovieShowtimes />} />
-        <Route path="/reserve/:showtimeId" element={<SeatReservation />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
